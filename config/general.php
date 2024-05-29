52d94c9f3677c1b911740b8fdc1be8b48a125b11
Dan <danielcrump@outlook.com>
2024-05-29 10:24:54 -0400

    change back to admin

--- config/general.php
+++ config/general.php
@@ -23,7 +23,7 @@ return [
       'omitScriptNameInUrls' => true,
 
       // Control Panel trigger word
-      'cpTrigger' => 'access',
+      'cpTrigger' => 'admin',
 
       // The secure key Craft will use for hashing and encrypting data
       'securityKey' => App::env('CRAFT_SECURITY_KEY'),
